b4963b27e633c99d23eeaab4615904c17a1ab24e
zzy <zhaozhenyu_17@163.com>
2018-03-30 14:45:46 +0800

    add trie data structure

---
+++ src/interview/tree/trie/Trie.java
@@ -0,0 +1,44 @@
+package interview.tree.trie;
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar,new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char currentChar = prefix.charAt(i);
+            if (node.containsKey(currentChar)) {
+                node = node.get(currentChar);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
